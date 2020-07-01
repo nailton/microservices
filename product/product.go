6d0b553076be6be064faeff779e7579b43e6ffbc
Nailton Sousa <nailton.osn@gmail.com>
2020-06-30 22:37:57 -0300

    wip get product by id

--- product/product.go
+++ product/product.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"net/http"
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
+
+type Product struct {
+	Uuid string `json:"uuid"`
+	Product string `json:"product"`
+	Price string `json:"price,string"`
+}
+
+type Products struct {
+	Products []Product
+}
 
 func loadData() []byte {
 	jsonFile, err := os.Open("products.json")
@@ -24,9 +35,25 @@ func ListProducts(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(products))
 }
 
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := loadData()
+
+	var products Products
+	json.Unmarshal(data, &products)
+
+	 for _, v := range products.Products {
+	 	if v.Uuid == vars["id"] {
+	 		product, _ := json.Marshal(v)
+	 		w.Write([]byte(product))
+	 	}
+	 }
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/products", ListProducts)
+	r.HandleFunc("/product/{id}", GetProductById)
 	http.ListenAndServe(":8081", r)
 }
 
